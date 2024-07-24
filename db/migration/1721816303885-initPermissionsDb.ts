d449ee76d39e7c3b510c22881d1e911d86260601
Yaroslav Klymovets <yaroslav.klymovets@gmail.com>
2024-07-24 16:33:35 +0300

    modify migration file

--- db/migration/1721816303885-initPermissionsDb.ts
+++ db/migration/1721816303885-initPermissionsDb.ts
@@ -1,27 +1,78 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
 
-export class SeedPermissions1721816303885 implements MigrationInterface {
+export class InitPermissionsDb1721816303885 implements MigrationInterface {
     name = 'initPermissionsDb1721816303885'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "resource_entity" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, CONSTRAINT "UQ_8df40c9e2ce0ce05d3a65347a80" UNIQUE ("name"))`);
-        await queryRunner.query(`CREATE TABLE "actions_entity" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, CONSTRAINT "UQ_bc13b73e5b8841da7e41f9d3120" UNIQUE ("name"))`);
-        await queryRunner.query(`CREATE TABLE "permissions_entity" ("id" varchar PRIMARY KEY NOT NULL, "action_id" varchar, "resource_id" varchar)`);
-        await queryRunner.query(`CREATE TABLE "temporary_permissions_entity" ("id" varchar PRIMARY KEY NOT NULL, "action_id" varchar, "resource_id" varchar, CONSTRAINT "FK_da2c04f0a4fb9ef4c949a2e1959" FOREIGN KEY ("action_id") REFERENCES "actions_entity" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT "FK_4dcbffbc2ca251b70642d4d189e" FOREIGN KEY ("resource_id") REFERENCES "resource_entity" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
-        await queryRunner.query(`INSERT INTO "temporary_permissions_entity"("id", "action_id", "resource_id") SELECT "id", "action_id", "resource_id" FROM "permissions_entity"`);
-        await queryRunner.query(`DROP TABLE "permissions_entity"`);
-        await queryRunner.query(`ALTER TABLE "temporary_permissions_entity" RENAME TO "permissions_entity"`);
-
+        await queryRunner.createTable(new Table({
+            name: 'actions',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    default: 'uuid_generate_v4()',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                    isUnique: true
+                }
+            ]
+        }));
+        await queryRunner.createTable(new Table({
+            name: 'resource',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    default: 'uuid_generate_v4()',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                    isUnique: true
+                }
+            ]
+        }));
+        await queryRunner.createTable(new Table({
+            name: 'permissions',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    default: 'uuid_generate_v4()',
+                },
+                {
+                    name: 'action_id',
+                    type: 'uuid'
+                },
+                {
+                    name: 'resource_id',
+                    type: 'uuid'
+                }
+            ]
+        }));
+        await queryRunner.createForeignKey('permissions', new TableForeignKey({
+            columnNames: ['action_id'],
+            referencedTableName: 'actions',
+            referencedColumnNames: ['id'],
+        }));
+        await queryRunner.createForeignKey('permissions', new TableForeignKey({
+            columnNames: ['resource_id'],
+            referencedTableName: 'resource',
+            referencedColumnNames: ['id'],
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "permissions_entity" RENAME TO "temporary_permissions_entity"`);
-        await queryRunner.query(`CREATE TABLE "permissions_entity" ("id" varchar PRIMARY KEY NOT NULL, "action_id" varchar, "resource_id" varchar)`);
-        await queryRunner.query(`INSERT INTO "permissions_entity"("id", "action_id", "resource_id") SELECT "id", "action_id", "resource_id" FROM "temporary_permissions_entity"`);
-        await queryRunner.query(`DROP TABLE "temporary_permissions_entity"`);
-        await queryRunner.query(`DROP TABLE "permissions_entity"`);
-        await queryRunner.query(`DROP TABLE "actions_entity"`);
-        await queryRunner.query(`DROP TABLE "resource_entity"`);
+        await queryRunner.dropForeignKey('permissions', 'FK_action_id');
+        await queryRunner.dropForeignKey('permissions', 'FK_resource_id');
+        await queryRunner.dropTable('permissions');
+        await queryRunner.dropTable('resource');
+        await queryRunner.dropTable('actions');
     }
 
 }
