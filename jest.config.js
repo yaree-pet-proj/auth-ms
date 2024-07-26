145a873423a19bc8a94650d3cc79f8016b510754
Yaroslav Klymovets <yaroslav.klymovets@gmail.com>
2024-07-27 00:21:39 +0300

    jest config aligned

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node'
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }]
+    },
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)']
 };
