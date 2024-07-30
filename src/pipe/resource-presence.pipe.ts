7408e59ed9dd4a7b31dace6649a150c21a7376e2
Yaroslav Klymovets <yaroslav.klymovets@gmail.com>
2024-07-30 19:44:51 +0300

    pipe validation presence of resource

---
+++ src/pipe/resource-presence.pipe.ts
@@ -0,0 +1,21 @@
+import {ArgumentMetadata, BadRequestException, Injectable, PipeTransform} from "@nestjs/common";
+import {ResourceService} from "../service/resource.service";
+
+@Injectable()
+export class ResourcePresencePipe implements PipeTransform {
+
+    constructor(
+        private readonly resourceService: ResourceService
+    ) {
+        this.resourceService = resourceService;
+    }
+
+    async transform(value: any) {
+        const resource = await this.resourceService.findOne(value);
+        if (!resource) {
+            throw new BadRequestException("Record doesn't exist");
+        }
+        return value;
+    }
+
+}
