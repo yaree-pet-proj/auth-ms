b26710ddbdec80ab71b7c403a85c8aed5738299f
Yaroslav Klymovets <yaroslav.klymovets@gmail.com>
2024-08-08 21:18:11 +0300

    sequelize to support seeding history

--- src/database/sequelize.config.js
+++ src/database/sequelize.config.js
@@ -6,6 +6,8 @@ const config = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    seederStorage: "sequelize",
+    seederStorageTableName: "SequelizeData",
     logging: true,
     define: {
         hooks: {
